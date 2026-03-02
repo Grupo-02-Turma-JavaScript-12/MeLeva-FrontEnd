71da44f79f026ae21ea7eb69712eed54f66169f7
allysonaggp <allysonaggp@gmail.com>
2026-03-02 09:58:23 -0300

    adicionado navbar,footer e home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import{BrowserRouter, Route, Routes}from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./assets/pages/Home";
+import NavBar from "./assets/components/navbar/NavBar";
+import Footer from "./assets/components/footer/Footer";
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route></Route>
-        </Routes>
+        <NavBar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
