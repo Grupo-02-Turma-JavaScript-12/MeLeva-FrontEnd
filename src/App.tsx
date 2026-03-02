324fdb39b887fd2bc81eb0357d4912223de6c2a4
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2026-03-02 10:20:49 -0300

    navbar criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import{BrowserRouter, Route, Routes}from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route></Route>
+          <Route />
         </Routes>
       </BrowserRouter>
     </>
